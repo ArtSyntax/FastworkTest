1c2ac8bfac387c9d1cc68f9a11a0aa5fbca85fd7
artvm <arttra_eagleb@hotmail.com>
2018-05-27 20:42:13 +0700

    update api response

--- mail_be/mailservice/models.py
+++ mail_be/mailservice/models.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Mail(models.Model):
-    to_mail = models.CharField(max_length=200)
-    from_mail = models.CharField(max_length=200)
+    to_mail = models.EmailField(max_length=200)
+    from_mail = models.EmailField(max_length=200)
     subject = models.CharField(max_length=200, blank=True, default='')
     text = models.CharField(max_length=5000, blank=True, default='')
 
--- mail_be/mailservice/views.py
+++ mail_be/mailservice/views.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from rest_framework.response import Response
 from rest_framework import generics
 from mailservice.models import *
 from mailservice.serializers import MailSerializer
+from rest_framework import status
 
 class MailList(generics.ListCreateAPIView):
     queryset = Mail.objects.all()
     serializer_class = MailSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({"message": "Successful sent email to " + serializer.data["to_mail"]}, status=status.HTTP_200_OK, headers=headers)
 
 
 class MailDetail(generics.RetrieveUpdateDestroyAPIView):
