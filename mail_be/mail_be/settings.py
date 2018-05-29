50141207eacf10371d3ae15f1487bd20ecd554b6
artvm <arttra_eagleb@hotmail.com>
2018-05-29 16:27:52 +0700

    fix cors on server

--- mail_be/mail_be/settings.py
+++ mail_be/mail_be/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'mailservice.apps.MailserviceConfig',
 ]
 
@@ -55,7 +56,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'localhost:3000',
+)
 
 ROOT_URLCONF = 'mail_be.urls'
 
